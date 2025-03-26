08956f3edf9562a91031c6f8d428e76c364e2450
aidanbeck <aidanmbeck@outlook.com>
2025-03-25 21:48:06 -0500

    fix variable names

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let itemCount = 5;
+let price = 20;
+let sentance = name + " bought " + itemCount + " items for $" + price + ".";
 
-console.log(d);
+console.log(sentance);
